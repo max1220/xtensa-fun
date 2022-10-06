e0d98f9d521c754a0f3d81da6ddcda4c0d655302
Max <max1220@users.noreply.github.com>
2022-10-06 13:01:04 +0200

    Added gdb start option to QEMU script

--- qemu.sh
+++ qemu.sh
@@ -2,9 +2,13 @@
 
 # normal boot is 0x12, flash mode is 0x0f
 strap_mode="0x12"
+extra_args="-serial tcp::5555,server,wait"
 if [ "${1}" = "flash" ]; then
 	echo "=== FLASH MODE ==="
 	strap_mode="0x0f"
+elif [ "${1}" = "gdb" ]; then
+	echo "=== GDB MODE ==="
+	extra_args="-s -S"
 else
 	echo "=== QEMU ==="
 fi
@@ -13,9 +17,9 @@ fi
 qemu/build/qemu-system-xtensa -nographic \
 	-machine esp32 \
 	-m 4M \
-	-serial tcp::5555,server,wait \
 	-global driver=esp32.gpio,property=strap_mode,value=${strap_mode} \
 	-drive file=flash_image.bin,if=mtd,format=raw \
 	-drive file=qemu_efuse.bin,if=none,format=raw,id=efuse \
 	-drive file=sdcard.img,if=sd,format=raw \
-	-global driver=nvram.esp32.efuse,property=drive,value=efuse
+	-global driver=nvram.esp32.efuse,property=drive,value=efuse \
+	${extra_args}
