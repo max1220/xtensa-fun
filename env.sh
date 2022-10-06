23e4ba4d33b99d3b33f24d00379f54b46e3ad4cf
Max <max1220@users.noreply.github.com>
2022-10-06 14:18:39 +0200

    Use cloned repositories for build; Improve esp-idf export

--- env.sh
+++ env.sh
@@ -4,13 +4,13 @@ cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"
 # if these directories are not found, the dependency is downloaded
 # and built automatically.
 export ESP_QEMU_DIR="$(realpath qemu)"
-export ESP_QEMU_GIT="https://github.com/espressif/qemu"
+export ESP_QEMU_GIT="https://github.com/max1220/qemu-esp32"
 
 export ESP_IDF_DIR="$(realpath esp-idf)"
 export ESP_IDF_GIT="https://github.com/espressif/esp-idf"
 
 export ESP_LINUX_DIR="$(realpath linux-xtensa)"
-export ESP_LINUX_GIT="https://github.com/jcmvbkbc/linux-xtensa -b xtensa-6.0-esp32"
+export ESP_LINUX_GIT="https://github.com/max1220/linux-xtensa -b xtensa-6.0-esp32-max1220-experiments"
 
 export ESP_BUILDROOT_DIR="$(realpath buildroot)"
 export ESP_BUILDROOT_GIT="https://github.com/buildroot/buildroot"
@@ -27,7 +27,9 @@ export CROSS_COMPILE="xtensa-buildroot-uclinux-uclibc-"
 export ESPPORT="socket://localhost:5555"
 
 # esp-idf environment variables
-[ -f "${ESP_IDF_DIR}/export.sh" ] && . "${ESP_IDF_DIR}/export.sh"
+if [ -z "${ESP_IDF_VERSION-}" ]; then
+	[ -f "${ESP_IDF_DIR}/export.sh" ] && . "${ESP_IDF_DIR}/export.sh"
+fi
 
 # add buildroot tools to path
 if [ -d "${ESP_BUILDROOT_DIR}" ]; then
